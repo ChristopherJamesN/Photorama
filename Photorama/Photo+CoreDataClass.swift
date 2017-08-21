7cd273ecc5cd81b6177c753f43dd5d9206d010bb
Chris <nady.christopher@gmail.com>
2017-08-21 14:07:34 -0500

    Add files via upload

---
+++ Photorama/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Christopher Nady on 8/7/17.
+//  Copyright © 2017 Nady Analytics, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
---
+++ Photorama/Photorama/TagDataSource.swift
@@ -0,0 +1,31 @@
+//
+//  TagDataSource.swift
+//  Photorama
+//
+//  Created by Christopher Nady on 8/8/17.
+//  Copyright © 2017 Nady Analytics, LLC. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TagDataSource: NSObject, UITableViewDataSource {
+    
+    var tags: [Tag] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tags.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        let tag = tags[indexPath.row]
+        cell.textLabel?.text = tag.name
+        
+        cell.accessibilityLabel = "Double tap to toggle selected"
+        
+        return cell
+    }
+}
